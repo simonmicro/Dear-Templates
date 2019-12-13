17d4c76738b8be2b7dc3a9d7954bd3d14fcccc4f
Simon Beginn <simon@simonmicro.de>
2019-12-13 15:10:09 +0100

    Added cert example generator script

---
+++ CA Certs/Generate CA certificate.sh
@@ -0,0 +1,16 @@
+echo "Step 1: Generate a Private Key"
+openssl genrsa -des3 -out server.key
+
+echo "Step 2: Generate a CSR (Certificate Signing Request)"
+openssl req -new -key server.key -out server.csr
+
+echo "Step 3: Remove Passphrase from Key"
+cp server.key server.key.org
+openssl rsa -in server.key.org -out server.key
+
+echo "Step 4: Generating a Self-Signed Certificate"
+openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+
+echo "Step 5: Installing the Private Key and Certificate (DO IT YOURSELF)"
+rm server.csr
+rm server.key.org
